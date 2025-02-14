08404366ab523ef8b0882328f3838ac5ac827acd
Samuel Oliveira <samueldsoliveira7@gmail.com>
2025-02-14 15:31:13 -0300

    Init the nestJS project with prisma and BD configuration SQLite

---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,22 @@
+import { INestApplication, Injectable, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit {
+  async onModuleInit() {
+    await this.$connect();
+  }
+
+  enableShutdownHooks(app: INestApplication) {
+    this.$on('beforeExit' as never, () => {
+      app
+        .close()
+        .then(() => {
+          console.log('App closed');
+        })
+        .catch((err) => {
+          console.error('Error closing app', err);
+        });
+    });
+  }
+}
