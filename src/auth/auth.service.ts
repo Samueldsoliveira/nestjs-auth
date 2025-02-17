59de56a378afa2e55def246a74e89bda7ccec52b
Samuel Oliveira <samueldsoliveira7@gmail.com>
2025-02-17 15:35:54 -0300

    adds user validation and integration with user module

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { UserService } from 'src/user/user.service';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
-  validateUser(email: string, password: string) {
-    throw new Error('Method not implemented.');
+  constructor(private readonly userService: UserService) {}
+
+  async validateUser(email: string, password: string) {
+    const user = await this.userService.findByEmail(email);
+
+    if (user) {
+      const isPasswordValid = await bcrypt.compare(password, user.password);
+
+      console.log('USER PASSWORD', user.password);
+      console.log('PASSWORD', password);
+
+      if (isPasswordValid) {
+        return {
+          ...user,
+          password: undefined,
+        };
+      }
+    }
+
+    throw new Error('Invalid email or password');
   }
 }
